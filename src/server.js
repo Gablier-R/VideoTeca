5b904b5aed0b3b4cc43c8e58d0ab82a0c1e94265
Gabriel Rodrigues P. Rocha <gabrielrodrigues1927@gmail.com>
2022-09-26 22:59:48 -0300

    initial

---
+++ src/server.js
@@ -0,0 +1,18 @@
+require('dotenv').config();
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+const connectToDatabase = require('./database');
+
+connectToDatabase();
+
+const app = express();
+const port = 3333;
+
+app.use(routes);
+
+app.listen(port, () => {
+    console.log(`⚡Backend started http://localhost:${port}`);
+} );
