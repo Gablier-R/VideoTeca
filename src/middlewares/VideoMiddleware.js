9fbb3e91150d854f7ced26817c731d3cc614a0b7
Gabriel Rodrigues P. Rocha <gabrielrodrigues1927@gmail.com>
2022-09-27 22:14:24 -0300

    update, delete, updatelike

---
+++ src/middlewares/VideoMiddleware.js
@@ -0,0 +1,26 @@
+const {validate: isUuid} = require("uuid");
+const Video = require("../models/Video");
+
+module.exports = {
+    async validateId(request, response, next) {
+
+        const {id} = request.params;
+
+        if( !isUuid(id)){
+            return response.status(400).json({error: "invalid ID."});
+        }
+
+        try {
+            const video = await Video.findById(id);
+            response.video = video;
+
+            if (!video){
+                return res.status(404).json({error: "Video not found."});
+            }
+
+        } catch (err) {
+            return response.status(500).json({error: err.message});
+        }
+        next();
+    },
+};
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const routes = express.Router();
 
+const videoController = require('./controllers/videoController');
+const VideoMiddleware = require('./middlewares/VideoMiddleware');
 
-routes.get("/",(request,response ) => response.send("Hello Word"));
+
+routes.get("/videos", videoController.index);
+routes.post("/videos", videoController.store);
+routes.put("/videos/:id", VideoMiddleware.validateId, videoController.update);
+routes.delete("/videos/:id", VideoMiddleware.validateId, videoController.delete);
+routes.patch("/videos/:id", VideoMiddleware.validateId, videoController.updateLike);
 
 module.exports = routes;
