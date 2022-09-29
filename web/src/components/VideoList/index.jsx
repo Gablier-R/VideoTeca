dfe17bbe43db3545a9af9ca5b35ad4e09f03de9e
Gabriel Rodrigues P. Rocha <gabrielrodrigues1927@gmail.com>
2022-09-28 23:43:26 -0300

    estatico

--- web/src/components/VideoList/index.jsx
+++ web/src/components/VideoList/index.jsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { Container ,VideoListWrapper } from './styles';
 
 import AddVideo from '../AddVideo';
+import Video from '../Video';
+import { trusted } from 'mongoose';
 
 export default function VideoList(){
 
@@ -10,6 +12,12 @@ export default function VideoList(){
         <Container>
 
             <VideoListWrapper>
+                <Video
+                    id={1}
+                    title="Test title"
+                    link ="https://www.youtube.com/watch?v=ExNSl9CKcPQ&list=PL8YNlUoOZkkY8wjaI2t8DfvysoysmMLCv&index=3"
+                    liked = {true}
+                />
                 <AddVideo/>
             </VideoListWrapper>
 
