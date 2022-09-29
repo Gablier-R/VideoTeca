c45fe2104e2a6ec98536dae28a56869174a652cd
Gabriel Rodrigues P. Rocha <gabrielrodrigues1927@gmail.com>
2022-09-28 22:34:52 -0300

    React- video e listvideo

---
+++ web/src/components/Header/styles.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const Container  = styled.header`
+
+    width: 100%;
+    height:50px;
+    padding: 0px 20px;
+
+    display: flex;
+    align-items: center;
+
+    h3{
+         color: #1e1e1e;
+    }
+`
+
